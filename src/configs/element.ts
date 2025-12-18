fe394d6e412a18e2dea50730c379d47a1af1e824
balge <wucb@8btc.com>
2025-12-18 14:04:29 +0800

    精简功能

--- src/configs/element.ts
+++ src/configs/element.ts
@@ -1,15 +1,3 @@
-export const ELEMENT_TYPE_ZH: { [key: string]: string } = {
-  text: '文本',
-  image: '图片',
-  shape: '形状',
-  line: '线条',
-  chart: '图表',
-  table: '表格',
-  video: '视频',
-  audio: '音频',
-  latex: '公式',
-}
-
 export const MIN_SIZE: { [key: string]: number } = {
   text: 40,
   image: 20,
@@ -19,4 +7,4 @@ export const MIN_SIZE: { [key: string]: number } = {
   video: 250,
   audio: 20,
   latex: 20,
-}
+};
