5978eaa61e7d751b28ef5a67d584cf82796aadce
balge <wucb@8btc.com>
2025-12-18 13:32:28 +0800

    精简功能

--- src/components.d.ts
+++ src/components.d.ts
@@ -1,7 +1,1 @@
-import type { Icons } from '@/plugins/icon'
-
-declare module 'vue' {
-  export type GlobalComponents = Icons
-}
-
-export {}
+export {};
--- src/views/components/element/AudioElement/AudioPlayer.vue
+++ src/views/components/element/AudioElement/AudioPlayer.vue
@@ -1,8 +1,5 @@
 <template>
-  <div 
-    class="audio-player"
-    :style="{ transform: `scale(${1 / scale})` }"
-  >
+  <div class="audio-player" :style="{ transform: `scale(${1 / scale})` }">
     <audio
       class="audio"
       ref="audioRef"
@@ -36,7 +33,7 @@
             class="volume-bar-wrap"
             @mousedown="handleMousedownVolumeBar()"
             @touchstart="handleMousedownVolumeBar()"
-            @click="$event => handleClickVolumeBar($event)"
+            @click="($event) => handleClickVolumeBar($event)"
           >
             <div class="volume-bar" ref="volumeBarRef">
               <div class="volume-bar-inner" :style="{ width: volumeBarWidth }">
@@ -48,19 +45,26 @@
       </div>
 
       <span class="time">
-        <span class="ptime">{{ptime}}</span> / <span class="dtime">{{dtime}}</span>
+        <span class="ptime">{{ ptime }}</span> /
+        <span class="dtime">{{ dtime }}</span>
       </span>
 
-      <div 
+      <div
         class="bar-wrap"
         ref="playBarWrapRef"
         @mousedown="handleMousedownPlayBar()"
         @touchstart="handleMousedownPlayBar()"
-        @mousemove="$event => handleMousemovePlayBar($event)"
+        @mousemove="($event) => handleMousemovePlayBar($event)"
         @mouseenter="playBarTimeVisible = true"
         @mouseleave="playBarTimeVisible = false"
       >
-        <div class="bar-time" :class="{ 'hidden': !playBarTimeVisible }" :style="{ left: playBarTimeLeft }">{{playBarTime}}</div>
+        <div
+          class="bar-time"
+          :class="{ hidden: !playBarTimeVisible }"
+          :style="{ left: playBarTimeLeft }"
+        >
+          {{ playBarTime }}
+        </div>
         <div class="bar">
           <div class="loaded" :style="{ width: loadedBarWidth }"></div>
           <div class="played" :style="{ width: playedBarWidth }">
@@ -73,216 +77,228 @@
 </template>
 
 <script lang="ts" setup>
-import { computed, ref, useTemplateRef } from 'vue'
-import message from '@/utils/message'
+import { computed, ref, useTemplateRef } from "vue";
 
-const props = withDefaults(defineProps<{
-  src: string
-  loop: boolean
-  autoplay?: boolean
-  scale?: number
-}>(), {
-  autoplay: false,
-  scale: 1,
-})
+const props = withDefaults(
+  defineProps<{
+    src: string;
+    loop: boolean;
+    autoplay?: boolean;
+    scale?: number;
+  }>(),
+  {
+    autoplay: false,
+    scale: 1,
+  }
+);
 
 const secondToTime = (second = 0) => {
-  if (second === 0 || isNaN(second)) return '00:00'
+  if (second === 0 || isNaN(second)) return "00:00";
 
-  const add0 = (num: number) => (num < 10 ? '0' + num : '' + num)
-  const hour = Math.floor(second / 3600)
-  const min = Math.floor((second - hour * 3600) / 60)
-  const sec = Math.floor(second - hour * 3600 - min * 60)
-  return (hour > 0 ? [hour, min, sec] : [min, sec]).map(add0).join(':')
-}
+  const add0 = (num: number) => (num < 10 ? "0" + num : "" + num);
+  const hour = Math.floor(second / 3600);
+  const min = Math.floor((second - hour * 3600) / 60);
+  const sec = Math.floor(second - hour * 3600 - min * 60);
+  return (hour > 0 ? [hour, min, sec] : [min, sec]).map(add0).join(":");
+};
 
 const getBoundingClientRectViewLeft = (element: HTMLElement) => {
-  return element.getBoundingClientRect().left
-}
+  return element.getBoundingClientRect().left;
+};
 
-const audioRef = useTemplateRef<HTMLAudioElement>('audioRef')
-const playBarWrapRef = useTemplateRef<HTMLElement>('playBarWrapRef')
-const volumeBarRef = useTemplateRef<HTMLElement>('volumeBarRef')
+const audioRef = useTemplateRef<HTMLAudioElement>("audioRef");
+const playBarWrapRef = useTemplateRef<HTMLElement>("playBarWrapRef");
+const volumeBarRef = useTemplateRef<HTMLElement>("volumeBarRef");
 
-const volume = ref(0.5)
-const paused = ref(true)
-const currentTime = ref(0)
-const duration = ref(0)
-const loaded = ref(0)
+const volume = ref(0.5);
+const paused = ref(true);
+const currentTime = ref(0);
+const duration = ref(0);
+const loaded = ref(0);
 
-const playBarTimeVisible = ref(false)
-const playBarTime = ref('00:00')
-const playBarTimeLeft = ref('0')
+const playBarTimeVisible = ref(false);
+const playBarTime = ref("00:00");
+const playBarTimeLeft = ref("0");
 
-const ptime = computed(() => secondToTime(currentTime.value))
-const dtime = computed(() => secondToTime(duration.value))
-const playedBarWidth = computed(() => currentTime.value / duration.value * 100 + '%')
-const loadedBarWidth = computed(() => loaded.value / duration.value * 100 + '%')
-const volumeBarWidth = computed(() => volume.value * 100 + '%')
+const ptime = computed(() => secondToTime(currentTime.value));
+const dtime = computed(() => secondToTime(duration.value));
+const playedBarWidth = computed(
+  () => (currentTime.value / duration.value) * 100 + "%"
+);
+const loadedBarWidth = computed(
+  () => (loaded.value / duration.value) * 100 + "%"
+);
+const volumeBarWidth = computed(() => volume.value * 100 + "%");
 
 const seek = (time: number) => {
-  if (!audioRef.value) return
+  if (!audioRef.value) return;
 
-  time = Math.max(time, 0)
-  time = Math.min(time, duration.value)
+  time = Math.max(time, 0);
+  time = Math.min(time, duration.value);
 
-  audioRef.value.currentTime = time
-  currentTime.value = time
-}
+  audioRef.value.currentTime = time;
+  currentTime.value = time;
+};
 
 const play = () => {
-  if (!audioRef.value) return
+  if (!audioRef.value) return;
 
-  paused.value = false
-  audioRef.value.play()
-}
+  paused.value = false;
+  audioRef.value.play();
+};
 
 const pause = () => {
-  if (!audioRef.value) return
+  if (!audioRef.value) return;
 
-  paused.value = true
-  audioRef.value.pause()
-}
+  paused.value = true;
+  audioRef.value.pause();
+};
 
 const toggle = () => {
-  if (paused.value) play() 
-  else pause()
-}
+  if (paused.value) play();
+  else pause();
+};
 
 const setVolume = (percentage: number) => {
-  if (!audioRef.value) return
+  if (!audioRef.value) return;
 
-  percentage = Math.max(percentage, 0)
-  percentage = Math.min(percentage, 1)
+  percentage = Math.max(percentage, 0);
+  percentage = Math.min(percentage, 1);
 
-  audioRef.value.volume = percentage
-  volume.value = percentage
-  if (audioRef.value.muted && percentage !== 0) audioRef.value.muted = false
-}
+  audioRef.value.volume = percentage;
+  volume.value = percentage;
+  if (audioRef.value.muted && percentage !== 0) audioRef.value.muted = false;
+};
 
 const handleDurationchange = () => {
-  duration.value = audioRef.value?.duration || 0
-}
+  duration.value = audioRef.value?.duration || 0;
+};
 
 const handleTimeupdate = () => {
-  currentTime.value = audioRef.value?.currentTime || 0
-}
+  currentTime.value = audioRef.value?.currentTime || 0;
+};
 
 const handlePlayed = () => {
-  paused.value = false
-}
+  paused.value = false;
+};
 
 const handleEnded = () => {
-  if (!props.loop) pause()
+  if (!props.loop) pause();
   else {
-    seek(0)
-    play()
+    seek(0);
+    play();
   }
-}
+};
 
 const handleProgress = () => {
-  loaded.value = audioRef.value?.buffered.length ? audioRef.value.buffered.end(audioRef.value.buffered.length - 1) : 0
-}
+  loaded.value = audioRef.value?.buffered.length
+    ? audioRef.value.buffered.end(audioRef.value.buffered.length - 1)
+    : 0;
+};
 
-const handleError = () => message.error('视频加载失败')
+const handleError = () => console.error("视频加载失败");
 
 const thumbMove = (e: MouseEvent | TouchEvent) => {
-  if (!audioRef.value || !playBarWrapRef.value) return
-  const clientX = 'clientX' in e ? e.clientX : e.changedTouches[0].clientX
-  let percentage = (clientX - getBoundingClientRectViewLeft(playBarWrapRef.value)) / playBarWrapRef.value.clientWidth
-  percentage = Math.max(percentage, 0)
-  percentage = Math.min(percentage, 1)
-  const time = percentage * duration.value
+  if (!audioRef.value || !playBarWrapRef.value) return;
+  const clientX = "clientX" in e ? e.clientX : e.changedTouches[0].clientX;
+  let percentage =
+    (clientX - getBoundingClientRectViewLeft(playBarWrapRef.value)) /
+    playBarWrapRef.value.clientWidth;
+  percentage = Math.max(percentage, 0);
+  percentage = Math.min(percentage, 1);
+  const time = percentage * duration.value;
 
-  audioRef.value.currentTime = time
-  currentTime.value = time
-}
+  audioRef.value.currentTime = time;
+  currentTime.value = time;
+};
 
 const thumbUp = (e: MouseEvent | TouchEvent) => {
-  if (!audioRef.value || !playBarWrapRef.value) return
+  if (!audioRef.value || !playBarWrapRef.value) return;
 
-  const clientX = 'clientX' in e ? e.clientX : e.changedTouches[0].clientX
-  let percentage = (clientX - getBoundingClientRectViewLeft(playBarWrapRef.value)) / playBarWrapRef.value.clientWidth
-  percentage = Math.max(percentage, 0)
-  percentage = Math.min(percentage, 1)
-  const time = percentage * duration.value
+  const clientX = "clientX" in e ? e.clientX : e.changedTouches[0].clientX;
+  let percentage =
+    (clientX - getBoundingClientRectViewLeft(playBarWrapRef.value)) /
+    playBarWrapRef.value.clientWidth;
+  percentage = Math.max(percentage, 0);
+  percentage = Math.min(percentage, 1);
+  const time = percentage * duration.value;
 
-  audioRef.value.currentTime = time
-  currentTime.value = time
+  audioRef.value.currentTime = time;
+  currentTime.value = time;
 
-  document.removeEventListener('mousemove', thumbMove)
-  document.removeEventListener('touchmove', thumbMove)
-  document.removeEventListener('mouseup', thumbUp)
-  document.removeEventListener('touchend', thumbUp)
-}
+  document.removeEventListener("mousemove", thumbMove);
+  document.removeEventListener("touchmove", thumbMove);
+  document.removeEventListener("mouseup", thumbUp);
+  document.removeEventListener("touchend", thumbUp);
+};
 
 const handleMousedownPlayBar = () => {
-  document.addEventListener('mousemove', thumbMove)
-  document.addEventListener('touchmove', thumbMove)
-  document.addEventListener('mouseup', thumbUp)
-  document.addEventListener('touchend', thumbUp)
-}
+  document.addEventListener("mousemove", thumbMove);
+  document.addEventListener("touchmove", thumbMove);
+  document.addEventListener("mouseup", thumbUp);
+  document.addEventListener("touchend", thumbUp);
+};
 
 const volumeMove = (e: MouseEvent | TouchEvent) => {
-  if (!volumeBarRef.value) return
-  const clientX = 'clientX' in e ? e.clientX : e.changedTouches[0].clientX
-  const percentage = (clientX - getBoundingClientRectViewLeft(volumeBarRef.value)) / 45
-  setVolume(percentage)
-}
+  if (!volumeBarRef.value) return;
+  const clientX = "clientX" in e ? e.clientX : e.changedTouches[0].clientX;
+  const percentage =
+    (clientX - getBoundingClientRectViewLeft(volumeBarRef.value)) / 45;
+  setVolume(percentage);
+};
 
 const volumeUp = () => {
-  document.removeEventListener('mousemove', volumeMove)
-  document.removeEventListener('touchmove', volumeMove)
-  document.removeEventListener('mouseup', volumeUp)
-  document.removeEventListener('touchend', volumeUp)
-}
+  document.removeEventListener("mousemove", volumeMove);
+  document.removeEventListener("touchmove", volumeMove);
+  document.removeEventListener("mouseup", volumeUp);
+  document.removeEventListener("touchend", volumeUp);
+};
 
 const handleMousedownVolumeBar = () => {
-  document.addEventListener('mousemove', volumeMove)
-  document.addEventListener('touchmove', volumeMove)
-  document.addEventListener('mouseup', volumeUp)
-  document.addEventListener('touchend', volumeUp)
-}
+  document.addEventListener("mousemove", volumeMove);
+  document.addEventListener("touchmove", volumeMove);
+  document.addEventListener("mouseup", volumeUp);
+  document.addEventListener("touchend", volumeUp);
+};
 
 const handleClickVolumeBar = (e: MouseEvent) => {
-  if (!volumeBarRef.value) return
-  const percentage = (e.clientX - getBoundingClientRectViewLeft(volumeBarRef.value)) / 45
-  setVolume(percentage)
-}
+  if (!volumeBarRef.value) return;
+  const percentage =
+    (e.clientX - getBoundingClientRectViewLeft(volumeBarRef.value)) / 45;
+  setVolume(percentage);
+};
 
 const handleMousemovePlayBar = (e: MouseEvent) => {
   if (duration.value && playBarWrapRef.value) {
-    const px = playBarWrapRef.value.getBoundingClientRect().left
-    const tx = e.clientX - px
-    if (tx < 0 || tx > playBarWrapRef.value.offsetWidth) return
+    const px = playBarWrapRef.value.getBoundingClientRect().left;
+    const tx = e.clientX - px;
+    if (tx < 0 || tx > playBarWrapRef.value.offsetWidth) return;
 
-    const time = duration.value * (tx / playBarWrapRef.value.offsetWidth)
-    playBarTimeLeft.value = `${tx - (time >= 3600 ? 25 : 20)}px`
-    playBarTime.value = secondToTime(time)
-    playBarTimeVisible.value = true
+    const time = duration.value * (tx / playBarWrapRef.value.offsetWidth);
+    playBarTimeLeft.value = `${tx - (time >= 3600 ? 25 : 20)}px`;
+    playBarTime.value = secondToTime(time);
+    playBarTimeVisible.value = true;
   }
-}
+};
 
 const toggleVolume = () => {
-  if (!audioRef.value) return
+  if (!audioRef.value) return;
 
   if (audioRef.value.muted) {
-    audioRef.value.muted = false
-    setVolume(0.5)
+    audioRef.value.muted = false;
+    setVolume(0.5);
+  } else {
+    audioRef.value.muted = true;
+    setVolume(0);
   }
-  else {
-    audioRef.value.muted = true
-    setVolume(0)
-  }
-}
+};
 
 defineExpose({
   toggle,
-})
+});
 </script>
 
 <style scoped lang="scss">
-
 .audio-player {
   width: 280px;
   height: 50px;
@@ -401,7 +417,7 @@ defineExpose({
       }
 
       .icon-content {
-        transition: all .2s ease-in-out;
+        transition: all 0.2s ease-in-out;
         opacity: 0.8;
         color: #fff;
       }
